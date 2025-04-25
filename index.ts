b3ba1a5007f0cdb5b5417bacc069cc33f2b5cd04
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2025-04-24 22:59:10 -0500

    Also export interfaces in library index. Closes #14 (#15)

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import { Some, None } from './lib/Maybe';
 import { Ok, Err } from './lib/Result';
+import { Maybe } from './lib/Maybe';
+import { Result } from './lib/Result';
 
 export { Some, None, Ok, Err };
+export { Maybe, Result };
